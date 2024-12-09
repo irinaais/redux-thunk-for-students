7150746810842ac3cc82209f942605ef2309108f
Irina Shorokhova <irinaais90@gmail.com>
2024-12-09 18:24:03 +0300

    fix: button for fetching users

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-
 import './App.css';
 import React from 'react';
 import { fetchCustomers } from './asyncAction/customers';
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch()
-  const users = useSelector(state => state)
+  const dispatch = useDispatch();
+  const users = useSelector(state => state);
 
   return (
     <div className = 'app'>
-      <button className='getUsersBtn' onClick={() => fetchCustomers()}>Получить пользователей</button>
+      <button
+        className='getUsersBtn'
+        onClick={() => dispatch(fetchCustomers())}
+      >
+        Получить пользователей
+      </button>
       {users.map((el,i) =>{
         return(<h1 key={i}>{el.name}</h1>)
       })}
